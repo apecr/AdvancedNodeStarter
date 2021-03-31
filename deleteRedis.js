7f351f46ae342f71758bab8dcf51f0cde74d6883
apecr <alberto.eyo@gmail.com>
2021-03-31 17:36:43 +0200

    eslint

--- deleteRedis.js
+++ deleteRedis.js
@@ -2,5 +2,4 @@ const redis = require('redis')
 const redisURL = 'redis://127.0.0.1:6379'
 const client = redis.createClient(redisURL)
 
-
 client.flushall(() => console.log('Deleted'))
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,7 +4,7 @@ const requireLogin = require('../middlewares/requireLogin')
 const Blog = mongoose.model('Blog')
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async(req, res) => {
+  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -13,7 +13,7 @@ module.exports = app => {
     res.send(blog)
   })
 
-  app.get('/api/blogs', requireLogin, async(req, res) => {
+  app.get('/api/blogs', requireLogin, async (req, res) => {
     const redis = require('redis')
     const redisURL = 'redis://127.0.0.1:6379'
     const client = redis.createClient(redisURL)
@@ -22,7 +22,7 @@ module.exports = app => {
 
     // Do we have any cached data in redis related to this query
     const cachedBlogs = await client.get(req.user.id)
-    if (cachedBlogs){
+    if (cachedBlogs) {
       console.log('Serving from cache')
       return res.send(JSON.parse(cachedBlogs))
     }
@@ -31,7 +31,6 @@ module.exports = app => {
 
     // if no, we need to respond to request and update our cache to store the data
 
-
     const blogs = await Blog.find({ _user: req.user.id })
 
     res.send(blogs)
@@ -39,7 +38,7 @@ module.exports = app => {
     client.set(req.user.id, JSON.stringify(blogs))
   })
 
-  app.post('/api/blogs', requireLogin, async(req, res) => {
+  app.post('/api/blogs', requireLogin, async (req, res) => {
     const { title, content } = req.body
 
     const blog = new Blog({
